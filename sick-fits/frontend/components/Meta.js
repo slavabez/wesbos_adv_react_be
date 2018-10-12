90c6d6c37172ead61999abb5bc79efd31662d826
Slava Bezgachev <bezgachev@gmail.com>
2018-10-12 22:10:05 +0600

    Finished next lesson

--- sick-fits/frontend/components/Meta.js
+++ sick-fits/frontend/components/Meta.js
@@ -5,9 +5,10 @@ const Meta = () => {
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
-      <meta rel="shortcut icon" href="/static/favicon.png" />
+      <meta rel="icon shortcut" href="/static/favicon.png" />
       <meta rel="stylesheet" type="text/css" href="/static/nprogress.css" />
       <title>Sick Fits!</title>
+
     </Head>
   );
 };
