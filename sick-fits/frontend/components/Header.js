4fb0a394dbe7655d584fc8624acaf3cfd3c5942c
Slava Bezgachev <bezgachev@gmail.com>
2018-10-12 23:30:54 +0600

    Another lesson

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Nav from "./Nav";
 import Link from "next/link";
 import styled from "styled-components";
+import Nav from "./Nav";
 
 const Logo = styled.h1`
   font-size: 4rem;
