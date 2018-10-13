7c88dcb0dd602c2dd50207ad4653c62dbbd09faa
Slava Bezgachev <bezgachev@gmail.com>
2018-10-13 23:11:15 +0600

    Finished section 3 I think

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,5 +1,12 @@
-const Query = {
+const { forwardTo } = require("prisma-binding");
 
+const Query = {
+  /*async items(parent, args, ctx, info) {
+    const items = await ctx.db.query.items();
+    return items;
+  }*/
+
+  items: forwardTo("db")
 };
 
 module.exports = Query;
