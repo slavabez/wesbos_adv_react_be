a60afd9dfe82fd323b45b871693d11b22d6c1d94
Slava Bezgachev <bezgachev@gmail.com>
2018-10-15 17:53:27 +0600

    Cross site error, need to fix

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -41,8 +41,23 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
-  uploadFile = e => {
-    console.log("Uploading file...");
+  uploadFile = async e => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "sickfits");
+
+    const res = await fetch("https://api.cloudinary.com/v1_1/slavalab/image/upload", {
+      method: "POST",
+      body: data
+    });
+
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    })
   };
 
   render() {
